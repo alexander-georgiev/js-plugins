19434b914c8b1bfdc55b068db82892efb4ec100e
Alexander Georgiev <a.georgiev.91@gmail.com>
2022-05-17 14:13:49 +0300

    Update inViewport.js

--- inViewport.js
+++ inViewport.js
@@ -14,3 +14,29 @@
         });
     };
 }(jQuery, window));
+//usage
+$(this).inViewport(function (px) {
+});
+                   
+//alternative
+$.fn.isOnScreen = function () {
+
+    var win = $(window);
+
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+//usage
+if ($('#footer').isOnScreen()) {
+}
